c604e8e404bae11cbc9f73866fd167f14386a2d6
Fran <francisco.criado@onezix.com>
2021-05-17 18:39:31 +0200

    Refactoring class components to functional components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,33 @@
 import React from "react";
 
-class Button extends React.Component {
-    
-    render() {
-        return (
-            <div className="button-container animate__animated animate__backInUp">
-                <button className="main-button" onClick={this.props.showForm}>
-                    <div className="icon-container">
-                        <i className='bx bx-plus'></i>
-                    </div>
-                </button>
-            </div>
-        );
-    };
 
-};
+const Button = (props) => {
+    return (
+        <div className="button-container animate__animated animate__backInUp">
+            <button className="main-button" onClick={props.showForm}>
+                <div className="icon-container">
+                    <i className='bx bx-plus'></i>
+                </div>
+            </button>
+        </div>
+    );
+}
+
+
+// class Button extends React.Component {
+    
+//     render() {
+//         return (
+//             <div className="button-container animate__animated animate__backInUp">
+//                 <button className="main-button" onClick={this.props.showForm}>
+//                     <div className="icon-container">
+//                         <i className='bx bx-plus'></i>
+//                     </div>
+//                 </button>
+//             </div>
+//         );
+//     };
+
+// };
 
 export default Button;
